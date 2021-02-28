aecb06a1eb78532300216492ca2a803d380c146d
raduxc44 <73448351+raduxc44@users.noreply.github.com>
2021-02-28 17:20:16 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 window.onload = function () {
+    let x;
+    let y;
+    let op;
+    const sum = (x, y) => {return x + y};
+    const dif = (x, y) => {return x - y};
+    const mul = (x, y) => {return x * y};
+    const div = (x, y) => {return x / y};
+    if(op === '+') {sum(x, y)}
+    else if (op === '-') {dif(x, y)}
+    else if (op === '*') {mul(x, y)}
+    else if (op === '/') {div(x, y)}
+
+
+
+
 
 }
